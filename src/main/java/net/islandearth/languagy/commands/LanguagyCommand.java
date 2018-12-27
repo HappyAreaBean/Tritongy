24e305ae9f6a7b058ca70b9be99cc234540dfffc
SamB440 <sam@islandearth.net>
2018-12-27 19:58:58 +0000

    Made updating async

--- src/main/java/net/islandearth/languagy/commands/LanguagyCommand.java
+++ src/main/java/net/islandearth/languagy/commands/LanguagyCommand.java
@@ -2,6 +2,7 @@ package net.islandearth.languagy.commands;
 
 import java.util.Arrays;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
@@ -60,7 +61,7 @@ public class LanguagyCommand extends BukkitCommand {
 						if (sender instanceof Player) {
 							Player player = (Player) sender;
 							if (player.isOp() || player.getUniqueId().toString().equals("4b319cd4-e827-4dcf-a303-9a3fce310755")) {
-								new Updater(plugin).update(player);
+								Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> new Updater(plugin).update(player));
 							} else {
 								player.sendMessage(ChatColor.RED + "You don't have permission to do this!");
 							}
